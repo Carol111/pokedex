95fe4b53a3891a46a4b9770200a881971910a027
Caroline Carvalho <carvalhocaroline111@gmail.com>
2022-03-02 18:43:25 -0300

    add icon and splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 import theme from 'src/theme/theme';
 
-import {StatusBar, useColorScheme} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+import {StatusBar} from 'react-native';
 import Routes from 'src/routes';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    SplashScreen.hide();
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'light-content'} />
       <Routes />
     </ThemeProvider>
   );
